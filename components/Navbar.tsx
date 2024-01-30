c777a6f9c361bfb3929e19bf8acad295db564c3a
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-01-29 16:20:19 -1100

    added functionality for menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,18 @@
-import { NAV_LINKS } from "@/constants"
-import Image from "next/image"
-import Link from "next/link"
-import Button from "./Button"
+"use client";
+
+// I added the functionality for the hamburger menu, with the index.jsx in the public folder to help facillitate its files.
+
+import { useState } from "react";
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "./Button";
+import { menu, close } from "../public";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
@@ -12,14 +21,18 @@ const Navbar = () => {
 
       <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
-          <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
             {link.label}
           </Link>
         ))}
       </ul>
 
       <div className="lg:flexCenter hidden">
-        <Button 
+        <Button
           type="button"
           title="Login"
           icon="/user.svg"
@@ -27,15 +40,40 @@ const Navbar = () => {
         />
       </div>
 
-      <Image 
-        src="menu.svg"
-        alt="menu"
-        width={32}
-        height={32}
-        className="inline-block cursor-pointer lg:hidden"
-      />
-    </nav>
-  )
-}
+      <div className="lg:hidden flex flex-1 justify-end items-center">
+        <Image
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] inline-block object-contain cursor-pointer"
+          onClick={() => setToggle(!toggle)}
+        />
 
-export default Navbar
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+        >
+          <ul className="list-none flex justify-end items-start flex-col gap-5">
+            {NAV_LINKS.map((link) => (
+              <Link
+                href={link.href}
+                key={link.key}
+                className={`${
+                  active === link.label ? "text-black" : "text-gray-50"
+                } regular - 16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition - all hover:font-bold`}
+                onClick={() => {
+                  setToggle(!toggle);
+                  setActive(link.label);
+                }}
+              >
+                {link.label}
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
