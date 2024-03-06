5a595e10b654811e0a29eddd109ff8044b8d8722
Sameer <meersj@gmail.com>
2024-03-06 19:32:14 +0545

    fixed typo

--- components/Features.tsx
+++ components/Features.tsx
@@ -27,11 +27,11 @@ const Features = () => {
             />
             <h2 className="bold-40 lg:bold-64">Our Features</h2>
           </div>
-          <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20">
+          <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mt-20 lg:gap-20">
             {FEATURES.map((feature) => (
-              <FeatureItem 
+              <FeatureItem
                 key={feature.title}
-                title={feature.title} 
+                title={feature.title}
                 icon={feature.icon}
                 description={feature.description}
               />
